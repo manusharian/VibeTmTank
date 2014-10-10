aebbbcd3750781dd8719fe6b70d3d354487b17a9
Iasmina Gruicin <igruicin@gmail.com>
2014-10-10 23:00:17 +0300

    Adding MainMenu Script.

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Main menu.
+/// Attached to the main camera.
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+	
+}
