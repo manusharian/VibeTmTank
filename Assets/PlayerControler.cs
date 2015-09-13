b7e7780466e2045acaee1f414cfe137c25ce6e77
Marian-Emanuel Ionascu <ionascu.marianemanuel@gmail.com>
2015-09-13 22:03:11 +0300

    Preliminary joystick and fire button added.

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float speed = 5;
+   
+    private Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start () {
+        myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 moveVector = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * speed;
+        bool isFiring = CrossPlatformInputManager.GetButtonDown("FireButton");
+        if (isFiring)
+        {
+            myBody.AddForce(new Vector2(20,20) * 2);
+        }
+        else
+        {
+            myBody.AddForce(moveVector);
+        }
+	}
+}
